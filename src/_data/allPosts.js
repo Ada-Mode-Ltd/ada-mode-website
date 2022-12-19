e7bc3f74024eaf0bd5a46474f1edd2be9ab4cb4c
Fershad Irani <27988517+fershad@users.noreply.github.com>
2022-12-19 16:09:22 +0800

    remove featured post

--- src/_data/allPosts.js
+++ src/_data/allPosts.js
@@ -2,15 +2,9 @@ const allPosts = require('../utils/getPosts');
 
 module.exports = async function() {
     let data = await allPosts();
-    let features = await data.filter(doc => doc.featured);
-    let feature = features.length > 0 ? features[0] : data[0];
-    let posts = data.filter(doc => doc._id !== feature?._id);
-
-    // combine feature and posts
-    let combined = [feature, ...posts];
 
     // Get an array of the uniques categories used in each post
-    let categories = posts.reduce((acc, post) => {
+    let categories = data.reduce((acc, post) => {
         post.categories.forEach(cat => {
             if (!acc.includes(cat.title)) {
                 acc.push(cat.title);
@@ -18,6 +12,6 @@ module.exports = async function() {
         });
         return acc;
     }, []);
-
-    return { feature, posts, categories, combined };
+    
+    return { data, categories };
 };
