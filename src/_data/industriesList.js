5139ac86f1e2a05ffb12a0ab076d93ed54329300
Michelle Barker <michellebarker1984@gmail.com>
2024-02-01 17:36:09 +0000

    Various page updates

---
+++ src/_data/industriesList.js
@@ -0,0 +1,29 @@
+module.exports = async function () {
+  return [
+    {
+      title: 'Civil Nuclear',
+      items: ['Nuclear Power', 'Decommissioning', 'New Nuclear'],
+      imageUrl: '/images/icon-nuclear.svg',
+    },
+    {
+      title: 'Renewables',
+      items: ['Wind', 'Tidal', 'Solar'],
+      imageUrl: '/images/icon-wind.svg',
+    },
+    {
+      title: 'Manufacturing',
+      items: ['Foundation Industries', 'FMCG'],
+      imageUrl: '/images/icon-manufacturing.svg',
+    },
+    {
+      title: 'Utilities',
+      items: ['Water', 'Networks'],
+      imageUrl: '/images/icon-buildings.svg',
+    },
+    {
+      title: 'Marine',
+      items: ['Shipping', 'Ports'],
+      imageUrl: '/images/icon-water.svg',
+    },
+  ]
+}
