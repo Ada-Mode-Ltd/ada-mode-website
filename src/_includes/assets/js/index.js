20135a420dd02551aae93564eeecbb063becd8ed
Michelle Barker <michellebarker1984@gmail.com>
2024-02-02 13:17:42 +0000

    Move JS to module

--- src/_includes/assets/js/index.js
+++ src/_includes/assets/js/index.js
@@ -1,7 +1,9 @@
+import sal from 'sal.js'
 import menu from './modules/menu.js'
 import accordion from './modules/accordion.js'
 
 window.addEventListener('DOMContentLoaded', () => {
   menu()
+  sal()
   accordion()
 })
