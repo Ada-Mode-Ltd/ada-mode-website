bb084ecfaff2d01222c5c03e3d7df3fc4abd6cdc
Michelle Barker <michellebarker1984@gmail.com>
2024-05-29 17:45:39 +0100

    Prevent error if post has no categories

--- src/_data/allPosts.js
+++ src/_data/allPosts.js
@@ -1,17 +1,19 @@
-const allPosts = require('../utils/getPosts');
+const allPosts = require('../utils/getPosts')
 
-module.exports = async function() {
-    let data = await allPosts();
+module.exports = async function () {
+  let data = await allPosts()
 
-    // Get an array of the uniques categories used in each post
-    let categories = data.reduce((acc, post) => {
-        post.categories.forEach(cat => {
-            if (!acc.includes(cat.title)) {
-                acc.push(cat.title);
-            }
-        });
-        return acc;
-    }, []);
-    
-    return { data, categories };
-};
+  // Get an array of the uniques categories used in each post
+  let categories = data.reduce((acc, post) => {
+    let postCategories = post.categories || []
+
+    postCategories.forEach((cat) => {
+      if (!acc.includes(cat.title)) {
+        acc.push(cat.title)
+      }
+    })
+    return acc
+  }, [])
+
+  return { data, categories }
+}
